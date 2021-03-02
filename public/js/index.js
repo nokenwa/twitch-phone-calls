68f48e4f31dfeaf7409a63fb302e3b5e8fb077fe
Nathaniel Okenwa <nokenwa@chatterboxcoder.me>
2021-03-02 15:21:28 +0000

    Refactored code to avoid duplicate event listeners

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,13 @@ const websocket = new WebSocket(`wss://${window.location.host}`);
 
 const notification = new Audio("/assets/notification.mp3")
 const alert = document.getElementById('alert');
+const callInfo = document.getElementById('call-info')
 
 document.addEventListener("DOMContentLoaded", async () => {
 	const response = await fetch('/token',{method: 'POST'});
 	const data = await response.json();
 	const device = new Twilio.Device(data.token);
+	let activeConnection;
 
 	device.on("ready", (device) => {
 		console.log("Browser Phone Device Operational");
@@ -15,43 +17,43 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 	device.on("incoming", (connection) => {
 		console.log('incoming', connection);
+		activeConnection = connection;
 		websocket.send(JSON.stringify({ message: 'incomingCall' }))
-
+		callInfo.innerText = "Incoming Phone Call..."
 		alert.classList.add('visible')
-		websocket.addEventListener("message", (event) => {
-			const data = JSON.parse(event.data);
-			switch (data.message) {
-				case "answer":
-					connection.accept();
-					break;
-				case "end":
-					connection.reject();
-					console.log('REJECT CALL');
-					alert.classList.remove('visible');
-					break;
-				default:
-					break;
-			}
-		});
 	});
 
 	device.on("connect", (connection) => {
 		console.log('connect', connection);
-		websocket.send(JSON.stringify({ message: 'incomingCall' }))
-	
-		websocket.addEventListener("message", (event) => {
-			const data = JSON.parse(event.data);
-			switch (data.message) {
-				case "end":
-					connection.disconnect();
-					console.log('DISCONNECT CALL');
-					alert.classlist.remove('visible');
-					break;
-				default:
-					break;
-			}
-		});
+		callInfo.innerText = "Phone Call Connected.."
 	});
+
+	device.on("disconnect", (connection) => {
+		console.log('disconnect', connection);
+		callInfo.innerText = "Call Ended";
+		alert.classList.remove('visible');
+	})
+
+	websocket.addEventListener("message", (event) => {
+		const data = JSON.parse(event.data);
+		switch (data.message) {
+			case "answer":
+				if (activeConnection.status() === 'pending') {
+					activeConnection.accept();
+				}
+				break;
+			case "end":
+				if (activeConnection.status() === 'pending') {
+					activeConnection.reject();
+				} else activeConnection.disconnect();
+				
+				break;
+			default:
+				break;
+		}
+	});
+
+
 });
 
 // UNCOMMENT IF YOU NEED TO PREVENT YOUR SERVER FROM SHUTTING DOWN DUE TO LACK OF REQUESTS
